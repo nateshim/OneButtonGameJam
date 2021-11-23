0b7b19611829b8ca5b15237be3601fb53539a486
Nathanael Shim <nathanaelshim@Nathanaels-Mac-mini.local>
2021-11-23 11:10:43 -0800

    adds color changing mechanic

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Color teal = new Color(0, 128, 128);
+
+    private Dictionary<string, int> colors = new Dictionary<string, int>()
+    {
+        {"Red", 10},
+        {"Teal", 11}
+    };
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.layer == colors["Red"]) {
+            GetComponent<SpriteRenderer>().color = Color.red;
+        } else if (other.gameObject.layer == colors["Teal"])
+        {
+            GetComponent<SpriteRenderer>().color = teal;
+        } 
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
